023d7a1a53ccc84d29e915f7df7ff2c76f278fe5
Blausher <you@example.com>
2026-01-24 17:45:54 +0300

    v1

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-from routers.users import router as user_router
+# from routers.users import router as user_router
+import uvicorn
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8003)
