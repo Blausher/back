a7b55e8ac2e432573976bbf21766f4a16fe927ba
Blausher <you@example.com>
2026-01-31 15:40:51 +0300

    add validation in model

--- models/advertisement.py
+++ models/advertisement.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Advertisement(BaseModel):
-    seller_id: int
+    seller_id: int = Field(ge=0)
     is_verified_seller: bool
-    item_id: int
-    name: str
-    description: str
-    category: int
-    images_qty: int
+    item_id: int = Field(ge=0)
+    name: str = Field(min_length=1)
+    description: str = Field(min_length=1)
+    category: int = Field(ge=0)
+    images_qty: int = Field(ge=0)
